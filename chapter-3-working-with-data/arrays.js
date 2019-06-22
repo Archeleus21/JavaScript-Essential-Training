4d7b5fe4c2bd04d2896f3ac3397b985761d37ed4
Roybal_A <Roybal.usmc@gmail.com>
2019-06-21 19:30:39 -0700

    basic javascript, variable and conditionals

---
+++ chapter-3-working-with-data/arrays.js
@@ -0,0 +1,11 @@
+//arrays
+
+var pens;
+pens = ["red", "blue", "green", "orange"];
+var fourthPen = pens[3];
+
+console.log(pens);
+
+var mixedBag = ["keys", 5, false, 41];
+
+console.log(mixedBag);
---
+++ chapter-3-working-with-data/conditionals.js
@@ -0,0 +1,17 @@
+//if statements
+var a = 5;
+var b = "5";
+var theNumberMatch;
+
+if( a === b )
+{
+  theNumberMatch = true;
+}
+else
+{
+  theNumberMatch = false;
+}
+
+console.log("The numbers match: " + theNumberMatch);
+//short hand if conditional
+a === b ? console.log("Match") : console.console.log("No Match");
