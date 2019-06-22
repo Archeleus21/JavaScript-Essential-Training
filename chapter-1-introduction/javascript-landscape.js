c1a2012b9620adf0b4135fcafffbfdb917e653b5
Roybal_A <Roybal.usmc@gmail.com>
2019-06-21 18:00:36 -0700

    javascript over view and background

---
+++ chapter-1-introduction/javascript-landscape.js
@@ -0,0 +1,27 @@
+//JavaScript and its related libraries and programs
+//JavaScript conforms to the ever-evolving ECMAScript standard
+  //ECMAScript 2015 is the new name for ES6
+  /*this is a specification for interpreting code and languages
+    like JavaScript.
+    browsers use the ECMAScript standard to interpret JavaScript.
+    */
+  /*JQuery is a library for JavaScript functions and commands that allow
+    us to to do fancy JavaScript things with less code.
+    */
+  //JavaScript Frameworks(libraries)
+    /*AngularJS, React, Vue.js and so on are front-end application
+      Frameworks(libraries) used to simplify the building of advanced
+      interactive web-based applications.
+      for instance, PHP generates the pages from the server and send them
+      to the browsers, where as, these Frameworks run in the browsers
+      and pull just the data from the server.
+      These make the websites run really fast but have drawbacks when
+      the JavaScript isnt working. */
+    //Node.js
+    /*JavaScript platform built on browser runtime environments.
+    Used to run adv operations and applications on servers and
+    computers using JavaScript. These tools allow us to use
+    JavaScript as a server site programming language which kind
+    of goes against the idea that JavaScript is something that
+    runs in the browser.
+    */
