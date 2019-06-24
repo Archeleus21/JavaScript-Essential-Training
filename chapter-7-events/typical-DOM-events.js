acd0e8c683b96fbfe705e3341e77a5943cc6c1bd
Roybal_A <Roybal.usmc@gmail.com>
2019-06-23 17:12:17 -0700

    events

---
+++ chapter-7-events/typical-DOM-events.js
@@ -0,0 +1,24 @@
+//typical events
+//browser level events
+/*
+Load - when the resource and dependents have finished loading
+Error - when a resource failed to load
+Online/Offline - self-explanatory
+Resize - when a viewport is resized
+Scroll - when a viewport is scrolled up/down/left/right
+*/
+
+//Common DOM events
+/*
+Focus - when an elements receives focus (clicked, tabbed to, etc.)
+Blur - when an elements loses focus (when working in forms and leaving one form field to another, etc.)
+Reset/Submit - form-specific events, when a reset or submit button is pressed
+Mouse events - click, mouseover, drag drop, mouse hover, etc.
+*/
+
+//other events
+/*
+Media events - relate to audio/video playback
+Progress events - monitor ongoing browser progress
+CSS transition events - transition start/run/end
+*/
